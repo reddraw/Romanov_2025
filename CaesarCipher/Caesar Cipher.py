c9958f0bcf9af25b814b77cec18e70c3dbf84045
reddraw <112946235+reddraw@users.noreply.github.com>
2025-04-15 18:09:56 +0300

    ver.2 modified

--- CaesarCipher/Caesar Cipher.py
+++ CaesarCipher/Caesar Cipher.py
@@ -4,28 +4,33 @@ def Cipher(slovo, kod):
         number = ord(slovo[i])
         if (number >= 32 and number<=64):
             slovo2 += slovo[i]
-        elif (number + kod < 123):
-            slovo2 += chr(number + kod)
-        elif (number + kod >= 123):
-            slovo2 += chr(number + kod - 26)
+        elif (number>96 and number<123):
+            if(number+kod<123):
+                slovo2 += chr(number + kod)
+            elif (number + kod >= 123):
+                slovo2 += chr(number + kod - 26)
+        elif(number<91 and number>64):
+            if(number+kod<91):
+                slovo2+=chr(number+kod)
+            elif(number+kod>=91):
+                slovo2+=chr(number+kod-26)
     return (slovo2)
 
 def Runner(show):
-    if(Cipher("bebra",4)=="fifve"):
+    if(Cipher("Bebra",4)=="Fifve"):
         show+=1
-    if(Cipher("among the stars i'm the only flying!",11)
-    =="lxzyr esp delcd t'x esp zywj qwjtyr!"):
+    if(Cipher("Among the stars I'm the only flying!",11)
+    =="Lxzyr esp delcd T'x esp zywj qwjtyr!"):
         show+=1
     if(Cipher("zhakwong amm", 8)=="hpisewvo iuu"):
         show+=1
     return show
 show=0
-slovo = str(input("Choose an option: 1) TEST to test, 2) any other word to cipher it"))
+slovo = str(input("Print in case you want: 1) TEST to test the program, 2) any other word to cipher it"))
 if(slovo=="TEST"):
     show=Runner(show)
-    print("number of Tests passed - ",show,"/3")
+    print("Number of Tests passed - ",show,"/3")
 else:
-    slovo = slovo.lower()
     kod = int(input("Input key int"))
     print(slovo, "- main word")
     print(Cipher(slovo, kod), "- ciphered word")
